71460f400e36cc29983d3ce3672e0f53d73e666a
GaelR12 <112199310+GaelR12@users.noreply.github.com>
2024-08-27 23:30:54 -0500

    Added hellome.php

---
+++ hellome.php
@@ -0,0 +1,14 @@
+<?php
+// Creating the Variable
+$name = "Gael Rueda";
+// Creating the Associative Array
+$info = array (
+    "degree" => "CIS Associates Degree",
+    "transportation" => "car",
+    "github_repo" => "https://github.com/GaelR12/PHP_Practice.git"
+);
+// Display name
+echo "My name is: " . $name . "\n";
+// Displaying the Associative Array ($info)
+print_r($info);
+?>
